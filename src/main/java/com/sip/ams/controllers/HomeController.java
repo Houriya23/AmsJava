400020906e12aeb849c0c44d63d38b6957a2eb34
Mami Houria <houria.mami@enicar.ucar.tn>
2022-03-21 21:37:30 +0100

    crud de candidat dans un array list

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody; //unused
@@ -17,9 +18,9 @@ public class HomeController {
 	static ArrayList<Candidat>lc;
 	static {
 		lc=new ArrayList<>();
-		Candidat C1=new Candidat(1,"houria","houria@gmail.com","27876678");
-		Candidat C2=new Candidat(2,"Amir","amir@gmail.com","1234567");
-		Candidat C3=new Candidat(3,"farah","farah@gmail.com","76589987");
+		Candidat C1=new Candidat(0,"houria","houria@gmail.com","27876678");
+		Candidat C2=new Candidat(1,"Amir","amir@gmail.com","1234567");
+		Candidat C3=new Candidat(2,"farah","farah@gmail.com","76589987");
 		lc.add(C1);lc.add(C2);lc.add(C3);
 	}
 
@@ -63,4 +64,35 @@ public class HomeController {
 		//return "infos:"+Id+" "+Nom+" "+Email+" "+Tel;
 		return "redirect:candidats";
 	}
+	@GetMapping("/show/{idC}")
+	@ResponseBody
+	public String show( @PathVariable("idC")int Id)
+	{    return "ID: "+Id;
+		//return"home/show";
+	}
+	
+	@GetMapping("/delete/{idC}")
+	public String delete( @PathVariable("idC")int Id)
+	{    lc.remove(Id);
+	return "redirect:../candidats";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
